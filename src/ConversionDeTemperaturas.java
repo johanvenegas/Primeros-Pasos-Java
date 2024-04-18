bc854a9aa219f55d8dce3fd91129d46ecbee634f
Johan Venegas <johanvn12@gmail.com>
2024-04-17 18:25:22 -0600

    Creacion de la primera clase.Y deafio de temperatura

---
+++ src/ConversionDeTemperaturas.java
@@ -0,0 +1,13 @@
+public class ConversionDeTemperaturas {
+    public static void main(String[] arg){
+
+        double celcius = 37.5;
+        double fahrenheit = (celcius * 1.8)+32;
+        int fahrenheitEntero= (int) fahrenheit;
+        String aviso = String.format("La temperatura en celcius es de : %f y la temperatura en Fahrenheit es de : %f",celcius,fahrenheit);
+        System.out.println(aviso);
+        System.out.println("Temperatura en Fahrenheit: "+fahrenheit);
+        System.out.println("Temperatura entero en Fahrenheit: "+fahrenheitEntero);
+    }
+}
+
