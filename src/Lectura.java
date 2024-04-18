c82a8897951b069a556d81bc5c21063ca0c87754
Johan Venegas <johanvn12@gmail.com>
2024-04-17 19:15:07 -0600

    ingreso de datos con Scanner en java.

---
+++ src/Lectura.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Lectura {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Escriba el nombre de tu pelicula favorita");
+        String pelicula = teclado.nextLine();
+
+        System.out.println("Ahora escribe la fecha de lanzamiento");
+        int fechaDeLanzamiento = teclado.nextInt();
+
+        System.out.println("Por ultimo dinos que nota le das a esta Pelicula");
+        double nota = teclado.nextDouble();
+
+        System.out.println(pelicula);
+        System.out.println(fechaDeLanzamiento);
+        System.out.println(nota);
+
+    }
+}
